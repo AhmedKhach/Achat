05362af96bebe700ff698ac721616b34c3a2eb59
haythem <zoghlemihaythem2@gmail.com>
2024-04-28 23:42:26 +0200

    unit test

--- src/test/java/tn/esprit/rh/achat/FournisseurTest.java
+++ src/test/java/tn/esprit/rh/achat/FournisseurTest.java
@@ -1,8 +1,10 @@
 package tn.esprit.rh.achat;
 
-import static org.junit.Assert.assertSame;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -10,6 +12,7 @@ import static org.mockito.Mockito.when;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,6 +22,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import tn.esprit.rh.achat.entities.DetailFournisseur;
 import tn.esprit.rh.achat.entities.Fournisseur;
+import tn.esprit.rh.achat.entities.SecteurActivite;
 import tn.esprit.rh.achat.repositories.DetailFournisseurRepository;
 import tn.esprit.rh.achat.repositories.FournisseurRepository;
 import tn.esprit.rh.achat.repositories.SecteurActiviteRepository;
@@ -53,6 +57,58 @@ public class FournisseurTest {
         Fournisseur fournisseurMock = mock(Fournisseur.class);
         when(fournisseurMock.getDetailFournisseur()).thenReturn(detailFournisseurMock);
         fournisseurServiceImpl.addFournisseur(fournisseurMock);
+        assertDoesNotThrow(() -> fournisseurServiceImpl.addFournisseur(fournisseurMock));
+    }
+
+    @Test
+    void testSaveDetailFournisseur() {
+        DetailFournisseur detailFournisseurMock = mock(DetailFournisseur.class);
+        Fournisseur fournisseurMock = mock(Fournisseur.class);
+        when(fournisseurMock.getDetailFournisseur()).thenReturn(detailFournisseurMock);
+        fournisseurServiceImpl.saveDetailFournisseur(fournisseurMock);
+        assertDoesNotThrow(() -> fournisseurServiceImpl.saveDetailFournisseur(fournisseurMock));
+
+    }
+
+    @Test
+    void testUpdateFournisseur() {
+        DetailFournisseur detailFournisseurMock = mock(DetailFournisseur.class);
+        when(detailFournisseurRepository.save(any(DetailFournisseur.class))).thenReturn(detailFournisseurMock);
+        Fournisseur fournisseurMock = mock(Fournisseur.class);
+        when(fournisseurMock.getDetailFournisseur()).thenReturn(detailFournisseurMock);
+        fournisseurServiceImpl.updateFournisseur(fournisseurMock);
+        assertDoesNotThrow(() -> fournisseurServiceImpl.updateFournisseur(fournisseurMock));
+    }
+
+    @Test
+    void testDeleteFournisseur() {
+        Long fournisseurId = 123L;
+        fournisseurServiceImpl.deleteFournisseur(fournisseurId);
+        assertDoesNotThrow(() -> fournisseurServiceImpl.deleteFournisseur(fournisseurId));
+
+    }
+
+    @Test
+    void testRetrieveFournisseur_WhenExists() {
+        Long fournisseurId = 123L;
+        Fournisseur mockFournisseur = new Fournisseur();
+        mockFournisseur.setCode(fournisseurId.toString());
+        mockFournisseur.setLibelle("Test Fournisseur");
+        when(fournisseurRepository.findById(fournisseurId)).thenReturn(Optional.of(mockFournisseur));
+        fournisseurServiceImpl.retrieveFournisseur(fournisseurId);
+        assertDoesNotThrow(() -> fournisseurServiceImpl.retrieveFournisseur(fournisseurId));    }
+
+    @Test
+    void testAssignSecteurActiviteToFournisseur() {
+        Long idSecteurActivite = 1L;
+        Long idFournisseur = 2L;
+        Fournisseur fournisseurMock = new Fournisseur();
+        SecteurActivite secteurActiviteMock = new SecteurActivite();
+        when(fournisseurRepository.findById(idFournisseur)).thenReturn(Optional.of(fournisseurMock));
+        when(secteurActiviteRepository.findById(idSecteurActivite)).thenReturn(Optional.of(secteurActiviteMock));
+        fournisseurServiceImpl.assignSecteurActiviteToFournisseur(idSecteurActivite, idFournisseur);
+        assertDoesNotThrow(() -> fournisseurServiceImpl.assignSecteurActiviteToFournisseur(idSecteurActivite, idFournisseur));
+
     }
 
 }
