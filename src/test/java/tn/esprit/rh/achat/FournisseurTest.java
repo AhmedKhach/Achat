e2edcfb1b0626a5bf234e44ab55a75c2bd3ee225
haythem <zoghlemihaythem2@gmail.com>
2024-04-28 22:34:00 +0200

    unit test

--- src/test/java/tn/esprit/rh/achat/FournisseurTest.java
+++ src/test/java/tn/esprit/rh/achat/FournisseurTest.java
@@ -3,10 +3,12 @@ package tn.esprit.rh.achat;
 import static org.junit.Assert.assertSame;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -15,6 +17,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import tn.esprit.rh.achat.entities.DetailFournisseur;
 import tn.esprit.rh.achat.entities.Fournisseur;
 import tn.esprit.rh.achat.repositories.DetailFournisseurRepository;
 import tn.esprit.rh.achat.repositories.FournisseurRepository;
@@ -41,6 +44,15 @@ public class FournisseurTest {
         List<Fournisseur> actualRetrieveAllfournisseurResult = fournisseurServiceImpl.retrieveAllFournisseurs();
         assertEquals(fournisseurList, actualRetrieveAllfournisseurResult); // Fix assertion
         assertTrue(actualRetrieveAllfournisseurResult.isEmpty());
+    }
+
+    @Test
+    void testAddFournisseur() {
+        DetailFournisseur detailFournisseurMock = mock(DetailFournisseur.class);
+        when(detailFournisseurMock.getDateDebutCollaboration()).thenReturn(new Date());
+        Fournisseur fournisseurMock = mock(Fournisseur.class);
+        when(fournisseurMock.getDetailFournisseur()).thenReturn(detailFournisseurMock);
+        fournisseurServiceImpl.addFournisseur(fournisseurMock);
     }
 
 }
